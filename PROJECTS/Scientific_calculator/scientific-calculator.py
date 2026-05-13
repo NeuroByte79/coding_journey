e520c184b50a0d37812e943738df51336d6cc356
NeuroByte79 <sk3419407@gmail.com>
2026-05-13 18:14:00 +0530

    Added scientific-calculator.py file version0.1

---
+++ PROJECTS/Scientific_calculator/scientific-calculator.py
@@ -0,0 +1,19 @@
+# version1.1
+# Simple calculator
+print("@@@@ Welcome to self made calculator $$$$$$ ")
+operator = input("Enter operator('+','-','*','/') : ")
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*' :
+    result = num1 * num2
+elif operator == '/' :
+    result = num1 / num2
+else:
+    print("Invalid choice!\nPlz Enter a valid option.")
+
+print(f'The {operator} is in between the {num1},{num2} is : {result}')
